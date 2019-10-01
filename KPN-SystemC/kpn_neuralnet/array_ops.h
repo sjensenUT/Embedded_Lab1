25b953461076fc3ccb223e6f91029a98619c5a67
Steffen T Jensen <sjensen@mario.ece.utexas.edu>
2019-09-30 21:16:04 -0500

    added merge layer code, fixed merge tiles function in array_ops

--- KPN-SystemC/kpn_neuralnet/array_ops.h
+++ KPN-SystemC/kpn_neuralnet/array_ops.h
@@ -1,5 +1,6 @@
+#include<string>
 int* calcPrevCoords(int coords[], int stride, int filterSize, int prevWidth, int prevHeight, std::string prevLayerType);
-float* getSubArray(float arr[], int coords[], int width, int height, int numChannels);
-float* horzCat(float arr1[], float arr2[], int width1, int width2, int height, int numChannels);
-float* vertCat(float arr1[], float arr2[], int width, int height1, int height2, int numChannels);
-float* mergeTiles(float **tiles, int widths[], int heights[], int numChannels);
+//float* getSubArray(float arr[], int coords[], int width, int height, int numChannels);
+//float* horzCat(float arr1[], float arr2[], int width1, int width2, int height, int numChannels);
+//float* vertCat(float arr1[], float arr2[], int width, int height1, int height2, int numChannels);
+float* mergeTiles(float **tiles, const int widths[], const int heights[], int numChannels);
