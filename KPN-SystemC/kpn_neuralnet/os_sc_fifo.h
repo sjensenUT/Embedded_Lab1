24f23616b6ebc2acf7fb0c3906088577a21f1e40
JacksonFarley <Jackson_Farley@baylor.edu>
2019-10-19 10:46:25 -0500

    made some changes to fifo. When applied many no known conversion errors

--- KPN-SystemC/kpn_neuralnet/os_sc_fifo.h
+++ KPN-SystemC/kpn_neuralnet/os_sc_fifo.h
@@ -11,12 +11,18 @@
 //Source Code Location: 
 //https://github.com/systemc/systemc-2.3/blob/master/src/sysc/communication/sc_fifo.h
 
+#ifndef OS_SC_FIFO_H
+#define OS_SC_FIFO_H
+
 #include <string>
 #include <iostream>
 #include <systemc.h>
 #include "../kahn_process.h"
 #include "os_channel.h"
 
+namespace sc_core {
+
+template <class T>
 class	os_sc_fifo : public sc_fifo
 {
 	public:
@@ -28,13 +34,17 @@ class	os_sc_fifo : public sc_fifo
 
     void read() override;
     void write() override;     
+
+
+    private: 
+    sc_fifo& operator = ( const sc_fifo<T>& );
 };
 
 
 template <class T>
 inline
 void
-sc_fifo<T>::read( T& val_ )
+os_sc_fifo<T>::read( T& val_ )
 {
     int task;
     while( num_available() == 0 ) {  
@@ -51,7 +61,7 @@ sc_fifo<T>::read( T& val_ )
 template <class T>
 inline
 void
-sc_fifo<T>::write( const T& val_ )
+os_sc_fifo<T>::write( const T& val_ )
 {
     int task; 
     while( num_free() == 0 ) {
@@ -63,3 +73,27 @@ sc_fifo<T>::write( const T& val_ )
     buf_write( val_ );
     request_update();
 }
+
+
+// maybe necessary for the operator
+
+operator T ()
+    { return read(); }
+
+
+sc_fifo<T>& operator = ( const T& a )
+    { write( a ); return *this; }
+
+
+template <class T>
+inline
+::std::ostream&
+operator << ( ::std::ostream& os, const sc_fifo<T>& a )
+{
+    a.print( os );
+    return os;
+}
+
+
+} // namespace sc_core
+#endif //OS_SC_FIFO_H
